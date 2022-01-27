fa7e4a123a1a18ae9a29f19181cb3390036d6604
mangenotwork <2912882908@qq.com>
2022-01-27 16:12:35 +0800

    add post file func

--- gathertool.go
+++ gathertool.go
@@ -8,8 +8,8 @@
 
 	Description : gathertool 轻量级爬虫，接口测试，压力测试框架
 	Author : ManGe
-	Version : v0.2
-	Date : 20211227
+	Version : v0.3
+	Date : 20220127
 
 */
 
@@ -17,8 +17,11 @@ package gathertool
 
 import (
 	"bytes"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 )
 
@@ -87,6 +90,49 @@ func NewPostForm(u string, data map[string]string, vs ...interface{}) *Context{
 		return nil
 	}
 	request.Header.Add("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+	return Req(request, vs...)
+}
+
+// POST File
+func PostFile(url, paramName, filePath string, vs ...interface{}) *Context {
+	f, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer func() { _ = f.Close() }()
+
+	uploadBody := &bytes.Buffer{}
+	writer := multipart.NewWriter(uploadBody)
+
+	fWriter, err := writer.CreateFormFile("uploadFile", filePath)
+	if err != nil {
+		Info("copy file writer %v", err)
+	}
+
+	_, err = io.Copy(fWriter, f)
+	if err != nil {
+		panic(err)
+	}
+
+	fieldMap := map[string]string{
+		paramName: filePath,
+	}
+
+	for k, v := range fieldMap {
+		_ = writer.WriteField(k, v)
+	}
+
+	err = writer.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	request, err := http.NewRequest(POST, urlStr(url), uploadBody)
+	if err != nil{
+		return nil
+	}
+
+	request.Header.Add("Content-Type", writer.FormDataContentType())
 	return Req(request, vs...)
 }
 
