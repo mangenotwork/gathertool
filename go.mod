2ffbd4db44ae47aa405af8130ebb107a8625a34e
ManGe <2912882908@qq.com>
2023-08-07 10:17:01 +0800

    add gomail

--- go.mod
+++ go.mod
@@ -15,5 +15,7 @@ require (
 	golang.org/x/net v0.5.0
 	golang.org/x/text v0.6.0
 	google.golang.org/grpc v1.51.0
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 	gopkg.in/yaml.v3 v3.0.1
 )
---
+++ mail.go
@@ -0,0 +1,62 @@
+package gathertool
+
+import (
+	"gopkg.in/gomail.v2"
+)
+
+type Mail struct {
+	From string
+
+	// 邮件授权码
+	AuthCode string
+
+	// QQ 邮箱： SMTP 服务器地址：smtp.qq.com（SSL协议端口：465/587, 非SSL协议端口：25）
+	// 163 邮箱：SMTP 服务器地址：smtp.163.com（SSL协议端口：465/994，非SSL协议端口：25）
+	Host string
+	Port int
+	C    *gomail.Dialer
+	Msg  *gomail.Message
+}
+
+func NewMail(host, from, auth string, port int) *Mail {
+	m := &Mail{
+		From:     from,
+		AuthCode: auth,
+		Host:     host,
+		Port:     port,
+	}
+	m.C = gomail.NewDialer(
+		m.Host,
+		m.Port,
+		m.From,
+		m.AuthCode,
+	)
+	m.Msg = gomail.NewMessage()
+	m.Msg.SetHeader("From", from)
+	return m
+}
+
+func (m *Mail) Title(title string) *Mail {
+	m.Msg.SetHeader("Subject", title)
+	return m
+}
+
+func (m *Mail) HtmlBody(body string) *Mail {
+	m.Msg.SetBody("text/html", body)
+	return m
+}
+
+func (m *Mail) Send(to string) error {
+	m.Msg.SetHeader("To", to)
+	return m.C.DialAndSend(m.Msg)
+}
+
+func (m *Mail) SendMore(to []string) error {
+	mgs := make([]*gomail.Message, 0)
+	for _, v := range to {
+		newMsg := m.Msg
+		newMsg.SetHeader("To", v)
+		mgs = append(mgs, newMsg)
+	}
+	return m.C.DialAndSend(mgs...)
+}
